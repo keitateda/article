e9d9a46d0115f9ab592c76af4dc7f3f3634cd26a
tateda.kei <tateda.kei@tateda>
2017-06-23 18:03:31 +0900

    Signed-off-by: tateda.kei <tateda.kei@tateda>

--- article/src/jp/co/comnic/lesson/webapp/article/dao/Basedao.java
+++ article/src/jp/co/comnic/lesson/webapp/article/dao/Basedao.java
@@ -1,9 +1,13 @@
 package jp.co.comnic.lesson.webapp.article.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class Basedao {
 	
@@ -11,8 +15,9 @@ public class Basedao {
 	protected EntityTransaction tx = em.getTransaction();
 	protected CriteriaBuilder builder = em.getCriteriaBuilder();
 	
-	
-	
-	
-	
+	protected <T> List<T> findAll(CriteriaQuery<T> query, Root<T> root ){
+		return em.createQuery(query.select(root)).getResultList();
+	}
 }
+	
+//	public void insert(Object entity) throw DaoException{
