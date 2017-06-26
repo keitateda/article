1ffe46e1078bf2b1b5dc016523a6a025900e6af0
tateda.kei <tateda.kei@tateda>
2017-06-26 10:12:32 +0900

    Signed-off-by: tateda.kei <tateda.kei@tateda>

--- article/src/jp/co/comnic/lesson/webapp/article/dao/Accountdao.java
+++ article/src/jp/co/comnic/lesson/webapp/article/dao/Accountdao.java
@@ -1,11 +1,14 @@
 package jp.co.comnic.lesson.webapp.article.dao;
 
-
-
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import jp.co.comnic.lesson.webapp.article.model.Account;
 
 //アカウントのログイン処理を実装するDAOクラス
 public class Accountdao extends Basedao{
 	
 	public Accountdao() throws DaoException{}
+	private CriteriaQuery<Account> query = builder.createQuery(Account.class);
+	
 	
 }
